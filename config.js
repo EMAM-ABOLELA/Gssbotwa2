1eb4a52ea94cb7dc1538368f7a002d1e02bfd6fa
EMAM-ABOLELA <160362844+EMAM-ABOLELA@users.noreply.github.com>
2024-07-04 12:04:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.owner = ['201144480436'];
+global.premium = ['201225016578'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "GOJO 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
